4b644f6780cbd72b92ab24a9c905067a782873d8
zakirey <66316804+zakirey@users.noreply.github.com>
2022-07-16 11:19:06 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://16aa1ac9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
